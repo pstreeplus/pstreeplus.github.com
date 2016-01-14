c837b6ff8b85c53ae1f6b31d5717e18d3b7bacc9
王志立 <wangzhili@teddymobile.cn>
2016-01-14 17:00:12 +0800

    "--all"

--- submit.sh
+++ submit.sh
@@ -1,5 +1,23 @@
-git status
-git add --all
-git status
-git commit -m "blog"
-git push origin master
+#! /bin/bash
+
+function main(){
+    files='--all'
+    comment='blog'
+    if [ $# -ne 0 ];then
+        files=$1
+        if [ $# -gt 1 ];then
+            comment=$2
+        fi
+    fi
+    git status
+    if [ ${files}x="--all"x ];then
+        git diff
+    else
+        git diff $files
+    fi
+    git add ${files}
+    git commit -m "\"${files}\""
+    git push origin master
+}
+
+main $*
